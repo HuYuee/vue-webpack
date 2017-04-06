baefde7c58583072d2dabdea4658a9cb96ca9faa
huyueb <1062887235@qq.com>
2017-04-06 11:37:53 +0800

    修改导航栏效果

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,53 +1,97 @@
 <template>
 <div class="head">
-<nav class="navbar navbar-default">
-<div class="container container-fluid">
-  <!-- Brand and toggle get grouped for better mobile display -->
-  <div class="navbar-header">
-    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+  <nav id="navbar" class="navbar navbar-default">
+    <div class="container container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
       <span class="sr-only">Toggle navigation</span>
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="">Brand</a>
-  </div>
+        <a class="navbar-brand" href="">Brand</a>
+      </div>
 
-  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-    <ul class="nav navbar-nav navbar-right">
-      <li :class="{active:isHome}"><router-link to="/">简历</router-link></li>
-      <li :class="{active:isWork}"><router-link to="/works">作品</router-link></li>
-      <li :class="{active:isLife}"><router-link to="/life">生活</router-link></li>
-    </ul>
-  </div>
-</div>
-</nav>
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+          <li :class="{active:isHome}">
+            <router-link to="/">简历</router-link>
+          </li>
+          <li :class="{active:isWork}">
+            <router-link to="/works">作品</router-link>
+          </li>
+          <li :class="{active:isLife}">
+            <router-link to="/life">生活</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </div>
 </template>
 
 <script>
+var $ = require('../../static/js/jquery')
+$(window).on('ready , scroll', function() {
+  if ($(window).scrollTop() > 30) {
+    $('#navbar').addClass('minified');
+  } else {
+    $('#navbar').removeClass('minified');
+  }
+});
 export default {
   props: ['name'],
-  data(){
+  data() {
     return {
-        age1:""
+      age1: ""
     }
   },
   computed: {
-    isHome: function(){
-        return this.name == "hello"
+    isHome: function() {
+      return this.name == "hello"
     },
-    isWork: function(){
-        return this.name == "work"
+    isWork: function() {
+      return this.name == "work"
     },
-    isLife: function(){
-        return this.name == "life"
+    isLife: function() {
+      return this.name == "life"
     }
   }
 }
 </script>
 
 <style scoped>
-.head{
+.navbar {
+  background-color: #28292e;
+  border: 0 none;
+  border-radius: 0;
+  position: fixed;
+  width: 100%;
+  padding: 15px 0;
+  margin-bottom: 0;
+  z-index: 10;
+  -webkit-transition: all .35s;
+  -moz-transition: all .35s;
+  -ms-transition: all .35s;
+  -o-transition: all .35s;
+  transition: all .35s;
+}
+
+.nav>.active>a,
+.nav>li>a:hover {
+  color: #0F64A6!important;
+}
+
+.nav>li>a {
+
+  background-color: transparent!important;
+}
+
+.navbar.minified {
+  background-color: #ffffff;
+  padding: 0;
+  position: fixed;
+  box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0), 0px 2px 10px 0px rgba(0, 0, 0, 0.08);
 }
 </style>
--- src/components/life-content.vue
+++ src/components/life-content.vue
@@ -37,6 +37,41 @@
   </div>
   <div class="timeline-row active">
     <div class="timeline-time">
+      <small><font><font>10月30日</font></font></small>
+      <font>
+        <font>下午4时53分
+        </font>
+      </font>
+    </div>
+    <div class="timeline-icon">
+      <div class="bg-primary">
+        <i class="fa fa-pencil"></i>
+      </div>
+    </div>
+    <div class="panel timeline-content">
+      <div class="panel-body">
+        <h2><font><font>
+            这是这个时间表后标题
+          </font></font></h2>
+        <p>
+          <font>
+            <font>
+              Lorem存有悲坐阿梅德，consectetuer adipiscing ELIT。</font>
+            <font>局船。</font>
+            <font>Curabitur aliquet NIBH NEC乌仁娜。</font>
+            <font>唯一没有，香蕉或蛋白质，其房地产与否。</font>
+            <font>但批准拉Laoreet关心的价格，狮子的宏观免费会员，需要现在坐在NIBH温度。</font>
+            <font>接力泵。</font>
+            <font>为了治疗。</font>
+            <font>智能手机。
+            </font>
+          </font>
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="timeline-row active">
+    <div class="timeline-time">
       <small><font><font>10月25日</font></font></small>
       <font>
         <font>下午6:14
